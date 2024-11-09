f5ad329777cfa8d481f5a553eb622acf1964ccbe
Caleb Curry <caleb@calebcurry.com>
2024-11-09 15:06:52 -0500

    1 improved

--- leetcode75/1.rs
+++ leetcode75/1.rs
@@ -1,9 +1,11 @@
 // 1768 - https://leetcode.com/problems/merge-strings-alternately/
 
+//creates chars() once and move through with .next()
+
 pub fn main() {
     println!(
         "{}",
-        Solution::merge_alternately(String::from("abcde"), String::from("pqr"))
+        Solution::merge_alternately(String::from("aėcde"), String::from("pqr"))
     );
 }
 
@@ -11,24 +13,23 @@ struct Solution {}
 
 impl Solution {
     pub fn merge_alternately(word1: String, word2: String) -> String {
-        let max = if word1.len() > word2.len() {
-            word1.len()
+        let max = if word1.chars().count() > word2.chars().count() {
+            word1.chars().count()
         } else {
-            word2.len()
-        };
-        let min = if word1.len() < word2.len() {
-            word1.len()
-        } else {
-            word2.len()
+            word2.chars().count()
         };
 
         let mut string = String::with_capacity(word1.len() + word2.len());
+
+        let mut chars1 = word1.chars();
+        let mut chars2 = word2.chars();
+
         for i in 0usize..max {
             if i < word1.len() {
-                string.push(word1.chars().nth(i).unwrap());
+                string.push(chars1.next().unwrap());
             }
             if i < word2.len() {
-                string.push(word2.chars().nth(i).unwrap());
+                string.push(chars2.next().unwrap());
             }
         }
 
